6a21bc94f5cf4d54f784f83c4e9dcf29a8226724
Chandler Bowman <doctorgrant99@gmail.com>
2023-09-13 10:24:03 -0600

    added success and warning components

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: `
+  <h2>Warning, Error.</h2>
+  `,
+  styleUrls: ['./warning-alert.component.css']
+})
+export class WarningAlertComponent {
+
+}
