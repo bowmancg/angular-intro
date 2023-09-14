f5d76fd817f74d5b5a93552b7a1c5e2dd78cb80c
Chandler Bowman <doctorgrant99@gmail.com>
2023-09-14 16:46:28 -0600

    styling with directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,23 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  allowNewServer = false
-  serverCreateStatus = 'No server created.'
-  serverName = 'Test Server'
+  allowNewServer = false;
+  serverCreateStatus = 'No server created.';
+  serverName = 'Test Server';
+  serverCreated = false;
+  servers = ['Test Server', 'Test Server 2']
 
   constructor() {
     setTimeout(() => {
-      this.allowNewServer = true
-    }, 2000)
+      this.allowNewServer = true;
+    }, 2000);
   }
 
   ngOnInit(): void {}
-  
+
   onCreateServer() {
-    this.serverCreateStatus = this.serverName + ' was Created.'
+    this.serverCreated = true;
+    this.servers.push(this.serverName)
+    this.serverCreateStatus = this.serverName + ' was Created.';
   }
 
   onUpdateServerName(event: Event) {
-    this.serverName = (<HTMLInputElement>event.target).value
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
